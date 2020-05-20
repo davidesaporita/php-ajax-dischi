64d61244765f51f278d1d0fc9af924866addbf5f
Davide Saporita <davide.saporita@gmail.com>
2020-05-20 17:54:33 +0200

    Main added + Data read from array

--- dist/partials/data/db.php
+++ dist/partials/data/db.php
@@ -1,6 +1,62 @@
 <?php 
 
+$collection = [
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ],
+    [
+        "cover" => "src/img/bach-secular-cantatas.jpg",
+        "title" => "Bach: The Secular Cantatas",
+        "author" => "Bach Collegium Japan",
+        "year" => "1998"
+    ]
 
+];
 
 
 
--- index.php
+++ index.php
@@ -22,7 +22,20 @@ $main_css = 'dist/assets/css/main.css';
             </div>
         </header>
         <main class="main-content">
-
+            <div class="container">
+                <ul class="album-list">
+                    <?php 
+                    foreach($collection as $item) {
+                        echo "<li class='album-list__item'>";
+                        echo    "<img class='album-list__item__cover' src='".$item['cover']."' alt=''>";
+                        echo    "<span class='album-list__item__title'>".$item['title']."</span>";
+                        echo    "<span class='album-list__item__author'>".$item['author']."</span>";
+                        echo    "<span class='album-list__item__year'>".$item['year']."</span>";
+                        echo "</li>";
+                    }
+                    ?>
+                </ul>
+            </div>
         </main>
         <footer class="footer">
 
