41dec4a72f9dc057b476ddd93bd850ec5ac8ed86
Davide Saporita <davide.saporita@gmail.com>
2020-05-20 17:03:24 +0200

    Header added

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php 
 
-    echo "test";
+include_once __DIR__ . '/dist/partials/data/db.php';
+$main_css = 'dist/assets/css/main.css';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $main_css; ?>">
+    <title>Php Ajax Dischi</title>
+</head>
+<body>
+    <div id="app">
+        <header class="header">
+            <div class="container">
+                <img class="header__logo" src="src/img/logo.png" alt="logo" />
+            </div>
+        </header>
+        <main class="main-content">
+
+        </main>
+        <footer class="footer">
+
+        </footer>
+    </div>
+</body>
+</html>
