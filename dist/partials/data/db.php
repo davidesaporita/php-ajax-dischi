52c9e6eed50a06f047a7060d5f6eab28c4f27b6d
Davide Saporita <davide.saporita@gmail.com>
2020-05-20 18:27:58 +0200

    Items (cards) style fixed

--- dist/partials/data/db.php
+++ dist/partials/data/db.php
@@ -5,55 +5,61 @@ $collection = [
         "cover" => "src/img/bach-secular-cantatas.jpg",
         "title" => "Bach: The Secular Cantatas",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2004"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/mozart-great-mass-in-c-minor.jpg",
+        "title" => "Mozart: Great Mass in C Minor",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2016"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-musikalisches-opfer.jpg",
+        "title" => "Bach - Musikalisches Opfer",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2017"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-cantatas-vol-55.jpg",
+        "title" => "J.S. Bach - Cantatas, Vol.55 (BWV 69, 30, 191)",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2013"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-for-japan.jpg",
+        "title" => "Bach for Japan",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2012"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-cantatas-vol-46.jpg",
+        "title" => "J.S. Bach - Cantatas, Vol.46 (BWV 102, 45, 17, 19)",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2010"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-the-passions.jpg",
+        "title" => "Bach - The Passions",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2002"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-cantatas-vol-38.jpg",
+        "title" => "J.S. Bach - Cantatas, Vol.38 (BWV 52, 82, 55, 58)",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "2008"
     ],
     [
-        "cover" => "src/img/bach-secular-cantatas.jpg",
-        "title" => "Bach: The Secular Cantatas",
+        "cover" => "src/img/bach-magnificat.jpg",
+        "title" => "J.S. Bach: Magnificat",
         "author" => "Bach Collegium Japan",
-        "year" => "1998"
+        "year" => "1999"
+    ],
+    [
+        "cover" => "src/img/handel-messiah.jpg",
+        "title" => "Händel - Messiah",
+        "author" => "Bach Collegium Japan",
+        "year" => "1997"
     ]
 
 ];
