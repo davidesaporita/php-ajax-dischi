1ad72913b641be58fcfe1e664f11138cf2d46e57
Davide Saporita <davide.saporita@gmail.com>
2020-09-14 17:35:27 +0200

    Fix

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+include_once __DIR__ . '/partials/data/db.php';
+$main_css = 'dist/assets/css/main.css';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $main_css; ?>">
+    <title>Php Ajax Dischi</title>
+</head>
+<body>
+    <div id="app">
+
+        <?php include_once __DIR__ . '/partials/templates/header.php' ?>
+        <?php include_once __DIR__ . '/partials/templates/main.php' ?>
+
+        <footer class="footer"></footer>
+        
+    </div>
+</body>
+</html>
--- partials/templates/header.php
+++ partials/templates/header.php
@@ -1,7 +1,7 @@
 <header class="header">
     <div class="container">
         <img class="header__logo" src="src/img/logo.png" alt="logo" />
-        <form action="index-php.php" method="GET">
+        <form action="index.php" method="GET">
             <input id="search" type="text" name="q" placeholder="Cerca per titolo, autore o anno">
         </form>        
     </div>
